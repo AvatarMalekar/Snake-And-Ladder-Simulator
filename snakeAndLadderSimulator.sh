8f1def08a00f1b602438774a5e081635262c9706
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-28 16:50:02 +0530

    ADDED:initialized variables for player and game board

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "---------------------------------------WELLCOME TO SNAKE AND LADDER SIMULATOR------------------------------------------------"
+SNAKE_AND_LADDER_GAME_BOARD=100;
+playerPosition=0;
+
