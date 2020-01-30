d5cb97c626d6a79c8a4aaa9561d3212173389d78
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-30 12:37:33 +0530

    ADDED:compatiblity for two players to play in game

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 echo "---------------------------------------WELLCOME TO SNAKE AND LADDER SIMULATOR------------------------------------------------"
+
+#CONSTANT
+PLAYER1=0
+PLAYER2=1
+
+#VARIABLES
+playingTurn=$PLAYER1
+player1Position=0
+player2Position=0
+player1CounterForDiceTossed=0
+player2CounterForDiceTossed=0
 declare -a snakeAndLadderArray
+
 for (( i=1; i<=100; i++ ))
 do
    snakeAndLadderArray[i]=0;
 done
-player1Position=0
-player2Position=0
-counterForDiceTossed=0
+#LADDERS
 snakeAndLadderArray[1]=38;
 snakeAndLadderArray[4]=14;
 snakeAndLadderArray[9]=31;
@@ -17,6 +27,8 @@ snakeAndLadderArray[36]=44;
 snakeAndLadderArray[51]=67;
 snakeAndLadderArray[71]=91;
 snakeAndLadderArray[80]=99;
+
+#SNAKES
 snakeAndLadderArray[16]=6;
 snakeAndLadderArray[49]=11;
 snakeAndLadderArray[62]=19;
@@ -27,13 +39,14 @@ snakeAndLadderArray[56]=53;
 snakeAndLadderArray[64]=60;
 snakeAndLadderArray[93]=73;
 snakeAndLadderArray[98]=78;
+
+#FUNCTIONS
 function rollDie(){
-   echo $((RANDOM%6+1))
+	echo $((RANDOM%6+1))
 }
 
-function snakeOrLadderOrNoPlay()
-{
-	for (( i=1; i<=100; i++ ))
+function snakeOrLadderOrNoPlay(){
+   for (( i=1; i<=100; i++ ))
    do
       if [ $i -eq $1 ]
       then
@@ -41,12 +54,11 @@ function snakeOrLadderOrNoPlay()
          break;
       elif [ $i -ne $1 -a $i -gt 99 ]
       then
-         var=$1
+			var=$1
       else
-         true
+			true
       fi
-
-done
+   done
    if [ $var -eq 0 ]
    then
       echo $1
@@ -54,44 +66,88 @@ done
       echo $var
    fi
 }
-function getplayerPosition()
-{
-  local player_position=$(($2+$1))
-   if [ $player_position -gt 100 ]
+function getplayerPosition(){
+ 	local currentPlayerPosition=$(($2+$1))
+   if [ $currentPlayerPosition -gt 100 ]
    then
-     local player_position=$(($player_position-$1))
+		local currentPlayerPosition=$(($currentPlayerPosition-$1))
    fi
-   echo $player_position
+   echo $currentPlayerPosition
 }
 function gameStatusDisplay()
 {
-if [ $1 -lt $2 ]
-   then
-      echo "CONGRATS..!! YOU GOT LADDER.."
+	if [ $1 -lt $2 ]
+	then
+   	echo "CONGRATS..!! YOU GOT LADDER.."
    elif [ $1 -gt $2 ]
    then
-echo "OOPS..!! YOU GOT SNAKE.."
+      echo "OOPS..!! YOU GOT SNAKE.."
    else
       echo "YOU ARE ON NO PLAY.."
    fi
-
-   echo "Player position is:" $2
-
+	echo  $3 "position is:" $2
 }
-while [ $player1Position -ne 100 ]
-do
-   value=$( rollDie )
+function playerOne()
+{
+   counterForSixP1=0
+	echo "***********************************************-:Its Player1 turn:-*********************************************************"
+	echo "press enter to toss a die"
+	read -s -n 1 key
+	value=$( rollDie )
    echo "value of dice :" $value
    player1Position=$(getplayerPosition $value $player1Position )
    tempPlayer1Position=$player1Position
    player1Position=$( snakeOrLadderOrNoPlay $tempPlayer1Position )
-   gameStatusDisplay $tempPlayer1Position $player1Position
-   read -s -n 1 key
-      ((counterForDiceTossed++))
+	gameStatusDisplay $tempPlayer1Position $player1Position $"Player1"
+   ((player1CounterForDiceTossed++))
    if [ $player1Position -eq 100 ]
    then
-      echo "YOU WIN"
+   	echo "PYAVER1 WIN"
    fi
+   if [ $value -eq 6 -a $counterForSixP1 -lt 2 ]
+   then
+		playingTurn=$PLAYER1
+      ((counterForSixP1++))
+   else
+      playingTurn=$PLAYER2
+   fi
+}
+
+function playerTwo(){
+   counterForSixP2=0
+   echo "*************************************************-:Its Player2 turn:-********************************************************"
+   echo "press enter to toss a die"
+   read -s -n 1 key
+   value=$( rollDie )
+   echo "value of dice :" $value
+   player2Position=$(getplayerPosition $value $player2Position )
+   tempPlayer2Position=$player2Position
+	player2Position=$( snakeOrLadderOrNoPlay $tempPlayer2Position )
+   gameStatusDisplay $tempPlayer2Position $player2Position $"Player2"
+   ((player2CounterForDiceTossed++))
+   if [ $player2Position -eq 100 ]
+   then
+ 		echo "PLAYER2 WIN"
+   fi
+   if [ $value -eq 6 -a $counterForSixP2 -lt 2 ]
+   then
+   	playingTurn=$PLAYER2
+      ((counterForSixP2++))
+   else
+      playingTurn=$PLAYER1
+   fi
+}
+
+#MAIN
+while [ $player1Position -ne 100 -a $player2Position -ne 100 ]
+do
+   if [ $playingTurn -eq $PLAYER1 ]
+   then
+   	playerOne
+   else
+   	playerTwo
+fi
 done
-echo "Number of times Dice was tossed is:" $counterForDiceTossed
+echo "Number of times Dice tossed by player1 is :" $player1CounterForDiceTossed
+echo "Number of times Dice tossed by player2 is :" $player2CounterForDiceTossed
 
